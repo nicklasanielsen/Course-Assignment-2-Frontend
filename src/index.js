d660b3233060d7b2ceea76244e482876629cba56
Nikolaj <cph-nl174@cphbusiness.dk>
2020-10-22 16:15:17 +0200

    Frontend Progress

--- src/index.js
+++ src/index.js
@@ -30,7 +30,6 @@ function fetchPersons() {
         });
 }
 
-function addPerson() {
     document.getElementById("savebtn").addEventListener("click", function () {
         let person = {
             "fullName": document.getElementById("fullName").value,
@@ -62,11 +61,122 @@ function addPerson() {
                 }
             });
     })
-}
 
-document.getElementById("addPerson").addEventListener("click", function () {
-    addPerson();
+    //SØG EFTER PERSON UD FRA TELEFONNUMMER
+
+document.getElementById("phoneForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+    let phoneNumber;
+    phoneNumber = document.getElementById("phoneField").value;
+    personFacade.getPersonByNumber(phoneNumber)
+    .then(data => {
+        const persons = data.all;
+        const personRows = persons.map(person => `
+    <tr>
+        <td>${person.fullName}</td>
+        <td>${person.email}</td>
+        <td>${person.address.address}</td>
+        <td>${person.address.city}</td>
+        <td>${person.phones.phoneNumber}</td>
+        <td>${person.hobbies.hobbyName}</td>
+    </tr>`
+        );
+        const personRowsAsString = personRows.join("");
+        document.getElementById("tableBody").innerHTML = personRowsAsString;
+    })
+    .catch(err => {
+        console.log(err);
+        if (err.status) {
+            err.fullError.then(e => document.getElementById("error").innerHTML = e.message);
+        } else {
+            console.log("Network error");
+        }
+    });
 })
 
+    //SØG EFTER PERSONER UD FRA HOBBY
+document.getElementById("hobbyForm").addEventListener("submit", function (e) {
+    e.preventDefault();
+    let hobby;
+    hobby = document.getElementById("hobbyField").value;
+    personFacade.getPersonsByHobby(hobby)
+    .then(data => {
+        const persons = data.all;
+        const personRows = persons.map(person => `
+    <tr>
+        <td>${person.fullName}</td>
+        <td>${person.email}</td>
+        <td>${person.address.address}</td>
+        <td>${person.address.city}</td>
+        <td>${person.phones.phoneNumber}</td>
+        <td>${person.hobbies.hobbyName}</td>
+    </tr>`
+        );
+        const personRowsAsString = personRows.join("");
+        document.getElementById("tableBody").innerHTML = personRowsAsString;
+    })
+    .catch(err => {
+        console.log(err);
+        if (err.status) {
+            err.fullError.then(e => document.getElementById("error").innerHTML = e.message);
+        } else {
+            console.log("Network error");
+        }
+    });
+})
 
+    //SØG EFTER PERSONER UD FRA BY
 
+document.getElementById("cityForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+    let city;
+    city = document.getElementById("cityField").value;
+    personFacade.getPersonByCity(city)
+    .then(data => {
+        const persons = data.all;
+        const personRows = persons.map(person => `
+    <tr>
+        <td>${person.fullName}</td>
+        <td>${person.email}</td>
+        <td>${person.address.address}</td>
+        <td>${person.address.city}</td>
+        <td>${person.phones.phoneNumber}</td>
+        <td>${person.hobbies.hobbyName}</td>
+    </tr>`
+        );
+        const personRowsAsString = personRows.join("");
+        document.getElementById("tableBody").innerHTML = personRowsAsString;
+    })
+    .catch(err => {
+        console.log(err);
+        if (err.status) {
+            err.fullError.then(e => document.getElementById("error").innerHTML = e.message);
+        } else {
+            console.log("Network error");
+        }
+    });
+})
+
+document.getElementById("postNumberButton").addEventListener("click", function(e) {
+    e.preventDefault();
+    personFacade.getAllPostnumbers()
+    .then(data => {
+        const persons = data.all;
+        const personRows = persons.map(person => `
+    <tr>
+        <td>${person.address.city.zip}</td>
+        <td>${person.address.city.cityName}</td>
+    </tr>`
+        );
+        const personRowsAsString = personRows.join("");
+        document.getElementById("tableBody").innerHTML = personRowsAsString;
+    })
+    .catch(err => {
+        console.log(err);
+        if (err.status) {
+            err.fullError.then(e => document.getElementById("error").innerHTML = e.message);
+        } else {
+            console.log("Network error");
+        }
+    });
+})
--- src/personFacade.js
+++ src/personFacade.js
@@ -1,26 +1,52 @@
-const URL = "localhost:8080/jpareststarter/api/person/"
+const URL = "http://localhost:8080/jpareststarter/api/person/"
 
 function getAllPersons(){
     return fetch(URL + "all")
     .then(handleHttpErrors);
 }
 
-function getPerson(id){
-    return fetch(URL + "id/" + id)
+function getPersonByNumber(phoneNumber){
+    return fetch(URL + "phone/" + phoneNumber)
+    .then(handleHttpErrors);
+}
+
+function getPersonsByHobby(hobby){
+    return fetch(URL + "xxx/" + hobby)
+    .then(handleHttpErrors);
+}
+
+function getPersonByCity(city){
+    return fetch(URL + "xxx/" + city)
+    .then(handleHttpErrors);
+}
+
+function getAllPostnumbers(){
+    return fetch(URL + "xxx")
     .then(handleHttpErrors);
 }
 
 function addPerson(person){
     const options = makeOptions("POST", person);
 
-    return fetch(url, options)
+    return fetch(URL, options)
+    .then(handleHttpErrors);
+}
+
+function deletePerson(id){
+    const options = makeOptions("DELETE", person)
+    
+    return fetch(URL + "xxx/" + id, options)
     .then(handleHttpErrors);
 }
 
 const personFacade = {
     getAllPersons,
-    getPerson,
-    addPerson
+    getPersonByNumber,
+    getPersonsByHobby,
+    getPersonByCity,
+    getAllPostnumbers,
+    addPerson,
+    deletePerson
 }
 
 function makeOptions(method, body) {
